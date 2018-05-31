998ab603e48b3c45f67b6145082b1fcc35e8c659
themanzi <christophermmanzi@protonmail.com>
2018-05-31 12:16:02 -0500

    linted

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 # This script sets up the host environment to run the application
 # in a docker stack. Loads the provided images.
 # TODO:
-# - 
+# - add docker auto install
 
 if [[ $# == 0 ]]; then
     echo "Please provide directory to export archive!"
