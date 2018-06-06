4d900dda001345cb8867b3acf276ced8e6030889
themanzi <christophermmanzi@protonmail.com>
2018-06-06 11:18:20 -0500

    fixed setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,24 +6,22 @@
 # - add docker auto install
 
 if [[ $# == 0 ]]; then
-    echo "Please provide directory to export archive!"
-    echo "Usage: setup.sh <archive_directory>"
+    echo "Please provide the export directory!"
+    echo "Usage: setup.sh <exports_directory>"
     exit 1
 fi
 
-if ! [[ -f $1/ramrodpcp-exports.tar.gz ]]; then
-    echo "${1}/ramrodpcp-exports.tar.gz not found!"
+if ! [[ -d $1 ]]; then
+    echo "Directory ${1} not found!"
     exit 2
 fi
-
-tar -xzvf $1/ramrodpcp-exports.tar.gz -C $1
 
 # Get image file names
 images=()
 
 while IFS= read -d $'\0' -r file ; do
      images+=( "$file" )
-done < <(find ./exports/ -name "*image-*" -print0)
+done < <(find $1 -name "*image-*" -print0)
 
 # Load images
 for img in "${images[@]}"; do
