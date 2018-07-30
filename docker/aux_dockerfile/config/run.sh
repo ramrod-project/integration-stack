e32d01dfeb597cd017b81fd5d026b43305836405
Dan Bauman <dan@bauman.space>
2018-07-30 15:19:07 -0500

    connect aux to brain

--- docker/aux_dockerfile/config/run.sh
+++ docker/aux_dockerfile/config/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # start nginx
+su www -c "python3 /brain_filesystem.py /www/files" &
+sleep 2
 nginx -g 'daemon off;' &
 
 # start vsftpd
+su fff -c "python3 /brain_filesystem.py /home/fff/files" &
+sleep 2
 /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
