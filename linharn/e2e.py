96dbb61e2603fe04cb6bec2d34b9cb2c2a2bc5f5
HDavisJr <wl7awc@gmail.com>
2018-08-31 14:50:00 -0500

    modified test script

--- linharn/e2e.py
+++ linharn/e2e.py
@@ -1,5 +1,7 @@
 from pytest import fixture, raises
-import selenium
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
 import multiprocessing
 
 from .linharn import control_loop
@@ -17,8 +19,40 @@ def linharn_client():
 def test_instantiate(linharn_client):
     """Test something...
     """
-    # exception testing
-    with raises(Exception):
-        raise Exception
-    # assert stuff
-    assert True
+
+    opts = Options()
+    opts.set_headless()
+    assert opts.headless  # Operating in headless mode
+    browser = Firefox(Options=opts)
+    browser.get("http://frontend:8080")
+
+    # test cases
+
+    add_tgt = browser.find_element_by_id('add_target_id')
+    add_tgt.click()
+
+    plgn = browser.find_element_by_id('plugin_name')
+    plgn.click()
+    plgn.send_keys('h')
+    plgn_opt = browser.find_element_by_id('1')
+    plgn_opt.click()
+    plgn.send_keys(Keys.ENTER)
+
+    tgt_ip - browser.find_element_by_id('location_num').send_keys('127.0.0.1')
+
+    tgt_port = browser.find_element_by_id('port_num')
+    tgt_port.send_keys('5000')
+    tgt_port.send_keys(Keys.ENTER)
+
+    tgt_name = browser.find_element_by_id('name_tag_id1')
+    tgt_name.click()
+
+    tgt_ip = browser.find_element_by_id('address_tag_id1')
+
+    add_job = browser.find_element_by_id('add_job_sc_id1')
+    add_job.click()
+
+    browser.implicitly_wait(10)
+
+    cmd_name = browser.find_element_by_id('acommandid6')
+    cmd_name.click()
