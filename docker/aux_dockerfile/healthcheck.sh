61ad4059fe4d5bf7ae28790953bcb98e31069f82
themanzi <christophermmanzi@protonmail.com>
2018-09-06 14:57:06 -0500

    removed unused dockerfiles

--- docker/aux_dockerfile/healthcheck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-netstat -ltnu | grep 21 || exit 1
-netstat -ltnu | grep 80 || exit 1
-echo "ls" | nc localhost 21 || exit 1
-echo "GET / HTTP/1.1" | nc localhost 80 || exit 1
-# netstat -ltnu | grep 53 || exit 1
