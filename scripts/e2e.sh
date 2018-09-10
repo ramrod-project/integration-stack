033b79c9112cd8493c0a52bae0d9e5f261fdca25
themanzi <christophermmanzi@protonmail.com>
2018-09-10 14:59:44 -0500

    fix

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -53,12 +53,13 @@ if (( counter > 44 )); then
     exit 1
 fi
 
+EXITCODE=0
 # get the selenium tests
 pip install -r ./integration-stack/linharn/requirements.txt
 pytest ./integration-stack/linharn/e2e.py
 # must exit upon test failure
 if ! [[ $? == 0 ]]; then
-    exit 1
+    EXITCODE=1
 fi
 
 # remove stack
@@ -66,3 +67,5 @@ docker stop selenium-firefox selenium-chrome
 docker stack rm pcp-test
 docker service rm AuxiliaryServices Harness-5000
 docker network prune -f
+
+exit EXITCODE
