6613d7c9b9bea0937e33ac5c05da3e8de701f874
mtrepid <matrippy@gmail.com>
2018-09-19 10:21:04 -0500

    Update e2e.py

--- linharn/e2e.py
+++ linharn/e2e.py
@@ -1,13 +1,30 @@
 from multiprocessing import Process
+from os import environ
+from time import sleep
 
 from pytest import fixture, raises
-from selenium.webdriver import Remote
-# from selenium.webdriver import ChromeOptions
+import rethinkdb as r
+from selenium.webdriver import Chrome, Firefox, Remote
+from selenium.webdriver import ChromeOptions
 from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
 
 from .linharn import control_loop
-# Run #1
+
+TARGET_TABLE = r.db("Brain").table("Targets")
+OUTPUT_TABLE = r.db("Brain").table("Outputs")
+JOBS_TABLE = r.db("Brain").table("Jobs")
+
+TABLES = [TARGET_TABLE, OUTPUT_TABLE, JOBS_TABLE]
+
+# Fixture will delete all jobs, targets, and outputs
+# before a test session from database.
+@fixture(scope="session", autouse=True)
+def clear_dbs():
+    conn = r.connect("frontend")
+    for table in TABLES:
+        table.delete().run(conn)
+    sleep(1)
 
 @fixture(scope="function")
 def linharn_client():
@@ -19,107 +36,137 @@ def linharn_client():
     yield client_thread
     client_thread.terminate()
 
-def test_instantiate_firefox(linharn_client):
+@fixture(scope="module")
+def chrome_browser():
+    # Connect to the Selenium server remove webdriver (Chrome)
+    no_headless = environ.get("NO_HEADLESS", "")
+    if no_headless == "TRUE":
+        browser = Firefox()
+    else:
+        browser = Remote("http://localhost:4445/wd/hub", DesiredCapabilities.CHROME.copy())
+    browser.implicitly_wait(20)
+    browser.get("http://frontend:8080")
+    yield browser
+    browser.close()
+
+@fixture(scope="module")
+def firefox_browser():
+    # Connect to the Selenium server remove webdriver (Firefox)
+    no_headless = environ.get("NO_HEADLESS", "")
+    if no_headless == "TRUE":
+        browser = Firefox()
+    else:
+        browser = Remote("http://localhost:4444/wd/hub", DesiredCapabilities.FIREFOX.copy())
+    browser.implicitly_wait(20)
+    browser.get("http://frontend:8080")
+    yield browser
+    browser.close()
+
+def test_instantiate_firefox(linharn_client, firefox_browser):
     """Test something...
     """
-    # Connect to the Selenium server remote webdriver (Firefox)
-    ff_browser = Remote("http://localhost:4444/wd/hub", DesiredCapabilities.FIREFOX.copy())
-    ff_browser.implicitly_wait(20)
-    
-    ff_browser.get("http://frontend:8080")
-    
+
     # Add a target
 
-    add_tgt = ff_browser.find_element_by_id('add_target_id')
+    add_tgt = firefox_browser.find_element_by_id('add_target_id')
     add_tgt.click()
 
-    plgn = ff_browser.find_element_by_id('service_name')
+    plgn = firefox_browser.find_element_by_id('service_name')
     plgn.click()
     plgn.send_keys('h')
     plgn.send_keys(Keys.ENTER)
 
-    ff_browser.find_element_by_id('location_num').send_keys('127.0.0.1')
+    firefox_browser.find_element_by_id('location_num').send_keys('127.0.0.2')
 
-    submit = ff_browser.find_element_by_id('add_target_submit')
+    submit = firefox_browser.find_element_by_id('add_target_submit')
     submit.click()
 
-def test_instantiate_chrome(linharn_client):
+def test_instantiate_chrome(linharn_client, chrome_browser):
     """Test something...
     """
-    
-    # Connect to the Selenium server remote webdriver (Chrome)
-    ch_browser = Remote("http://localhost:4445/wd/hub", DesiredCapabilities.CHROME.copy())
-    ch_browser.implicitly_wait(20)
-    
-    ch_browser.get("http://frontend:8080")
-    
     # Add a target
 
-    add_tgt = ch_browser.find_element_by_id('add_target_id')
+    add_tgt = chrome_browser.find_element_by_id('add_target_id')
     add_tgt.click()
 
-    plgn = ch_browser.find_element_by_id('service_name')
+    plgn = chrome_browser.find_element_by_id('service_name')
     plgn.click()
     plgn.send_keys('h')
     plgn.send_keys(Keys.ENTER)
 
-    ch_browser.find_element_by_id('location_num').send_keys('127.0.0.1')
+    chrome_browser.find_element_by_id('location_num').send_keys('127.0.0.1')
 
-    submit = ch_browser.find_element_by_id('add_target_submit')
+    submit = chrome_browser.find_element_by_id('add_target_submit')
     submit.click()
     
     # Add a job. Run job is a separate test case. Test both Firefox and Chrome.
 
-def test_instantiate_addjob0(linharn_client):
+def test_instantiate_addjob0(linharn_client, firefox_browser):
     """Test something...
     """
-    
-    # Connect to the Selenium server remote webdriver (Firefox)
-    ff_browser = Remote("http://localhost:4444/wd/hub", DesiredCapabilities.FIREFOX.copy())
-    ff_browser.implicitly_wait(20)
-    
-    ff_browser.get("http://frontend:8080")
-    
     # add job from plugin list
-    tgt_name = ff_browser.find_element_by_id('name_tag_id0').get_attribute('Harness')
-    #tgt_name.click()
+    tgt_name = firefox_browser.find_element_by_id('name_tag_id0')
+    tgt_name.click()
+    tgt_name.get_attribute('Harness')
 
-    tgt_ip = ff_browser.find_element_by_id('address_tag_id0').get_attribute('127.0.0.2')
+    tgt_ip = firefox_browser.find_element_by_id('address_tag_id0').get_attribute('127.0.0.2')
 
-    add_job = ff_browser.find_element_by_id('add_job_sc_id0')
+    add_job = firefox_browser.find_element_by_id('add_job_sc_id0')
     add_job.click()
 
-    ff_browser.implicitly_wait(10)
+    firefox_browser.implicitly_wait(10)
 
-    plugin = ff_browser.find_element_by_id('pluginid1').get_attribute('Harness:5000')
+    plugin = firefox_browser.find_element_by_id('pluginid1').get_attribute('Harness:5000')
 
-    addr = ff_browser.find_element_by_id('addressid1').get_attribute('127.0.0.2')
+    addr = firefox_browser.find_element_by_id('addressid1').get_attribute('127.0.0.2')
 
 
 # Run same test using Chrome
 
-def test_instantiate_addjob1(linharn_client):
+def test_instantiate_addjob1(linharn_client, chrome_browser):
     """Test something...
     """
-    
-    # Connect to the selenium server remote webdriver (Chrome)
-    ch_browser = Remote("http://localhost:4445/wd/hub", DesiredCapabilities.CHROME.copy())
-    ch_browser.implicitly_wait(20)
-    
-    ch_browser.get("http://frontend:8080")
 
     # add job from plugin list
-    tgt_name = ch_browser.find_element_by_id('name_tag_id1').get_attribute('Harness')
+    tgt_name = chrome_browser.find_element_by_id('name_tag_id1')
     tgt_name.click()
+    tgt_name.get_attribute('Harness')
 
-    tgt_ip = ch_browser.find_element_by_id('address_tag_id1').get_attribute('127.0.0.1')
+    tgt_ip = chrome_browser.find_element_by_id('address_tag_id1').get_attribute('127.0.0.1')
 
-    add_job = ch_browser.find_element_by_id('add_job_sc_id1')
+    add_job = chrome_browser.find_element_by_id('add_job_sc_id1')
     add_job.click()
 
-    ch_browser.implicitly_wait(10)
+    chrome_browser.implicitly_wait(10)
 
-    plugin = ch_browser.find_element_by_id('pluginid1').get_attribute('Harness:5000')
+    plugin = chrome_browser.find_element_by_id('pluginid1').get_attribute('Harness:5000')
 
-    addr = ch_browser.find_element_by_id('addressid1').get_attribute('127.0.0.1')
+    addr = chrome_browser.find_element_by_id('addressid1').get_attribute('127.0.0.1')
+    
+    # Add commands to existing job
+# Using Firefox browser
 
+def test_instantiate_addcmd0(linharn_client):
+    """Test something...
+    """
+
+    # Connect to the Selenium server remote webdriver (Firefox)
+    firefox_browser = Remote("http://localhost:4444/wd/hub", DesiredCapabilities.FIREFOX.copy())
+    firefox_browser.implicitly_wait(20)
+
+    firefox_browser.get("http://frontend:8080")
+
+    # bring up the Harness command list
+    tgt_name = firefox_browser.find_element_by_id('name_tag_id0')
+    tgt_name.click()
+    
+    cmd_name = firefox_browser.find_element_by_id('acommandid4')
+    cmd_name.click()
+
+    cmd_txt = firefox_browser.find_element_by_id('argumentid_0').send_keys('test1234')
+
+    cmd_btn = firefox_browser.find_element_by_id('add_command_to_job_id2')
+    cmd_btn.click()
+
+    cmd_box = firefox_browser.find_element_by_id('commandid1').get_attribute('test1234')
+    
