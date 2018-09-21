066a8a9172be45508dc3a10bb9b9f560021595f3
HDavisJr <38355954+HDavisJr@users.noreply.github.com>
2018-09-21 11:31:45 -0500

    Update e2e.py

--- linharn/e2e.py
+++ linharn/e2e.py
@@ -176,7 +176,7 @@ def test_instantiate_chkjob0(linharn_client):
 
     firefox_browser.get("http://frontend:8080")
  	  
-    job_done = firefox_browser.find_element_by_id('updatecontent1').get_attribute('test1234')
+    job_done = firefox_browser.find_element_by_id('jobstatusid1')
 
 #------------------------------------------------------------------------------    
 # Begin Chrome tests
