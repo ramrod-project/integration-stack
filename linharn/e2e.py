783188850ad6430d18f1ce22c1291f0a8f46b3f5
HDavisJr <38355954+HDavisJr@users.noreply.github.com>
2018-10-05 07:42:42 -0500

    Update e2e.py

--- linharn/e2e.py
+++ linharn/e2e.py
@@ -183,11 +183,9 @@ def test_instantiate_addjob1(linharn_client, chrome_browser):
 
     addr = chrome_browser.find_element_by_id('addressid1').get_attribute('127.0.0.1')
 
-        # Add a command to existing job
 
 def test_instantiate_addcmd1(linharn_client, chrome_browser):
 
-    # bring up the Harness command list
     tgt_name = chrome_browser.find_element_by_id('name_tag_id1')
     tgt_name.click()
 
